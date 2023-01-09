4b90e2f6f876c16d73cefe0f4b6cf26cc8a7976f
Marcus King <marcus.king@pm.me>
2023-01-08 23:49:53 -0800

    Update to index.html and script.js

--- Week-1/script.js
+++ Week-1/script.js
@@ -1,1 +1,1 @@
-var firstName = "Marcus", lastName = "King", yearOfStudy = 1, goal = "to obtain flexible employment that supplements and supports my continuing education.";
+var myFirstName = "Marcus", myLastName = "King", myYearOfStudy = 1, myGoal = "to obtain flexible employment that supplements and supports my continuing education.";
