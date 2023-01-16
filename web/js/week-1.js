027c51cf5d47c558201887a32fc791bf97f2ce45
Marcus King <92552389+marcus-king@users.noreply.github.com>
2023-01-15 19:09:56 -0800

    Update week-1.js

--- web/js/week-1.js
+++ web/js/week-1.js
@@ -1,19 +1,2 @@
-const firstName = "Marcus", lastName = "King", yearOfStudy = "first year", goal = "to obtain flexible employment that supplements and supports my continuing education.";
-
-function myFullName(){
-    document.getElementById("myFullName").innerHTML = string(firstName + " " + lastName);
-};
-
-function myYearOfStudy(){
-    document.getElementById("myYearOfStudy").innerHTML = yearOfStudy;
-};
-
-function myGoal(){
-        document.body.getElementsById("myGoal") = goal;
-};
-
-function populatePage(){
-    myFullName();
-    myYearOfStudy();
-    myGoal();
-}
+let firstName = "Marcus", lastName = "King", yearOfStudy = "first year", goal = "to obtain flexible employment that supplements and supports my continuing education.";
+const fullName = firstName + " " + lastName;
